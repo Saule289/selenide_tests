c25b6153ca5d3d3249669477281831c13769c5b2
Saule289 <saule289@mail.ru>
2023-01-11 19:44:09 +0400

    -homework2

---
+++ src/test/java/DragandDrop.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragandDrop {
+
+    @Test
+    void moveRectangle() {
+
+         // - Откройте https://the-internet.herokuapp.com/drag_and_drop
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        //- Перенесите прямоугольник А на место В
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-b").shouldHave(text("A"));
+
+    }
+}
---
+++ src/test/java/OpenPageCheck.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class OpenPageCheck {
+
+    @Test
+    void checkOpenPage() {
+
+        open("https://github.com/");
+        $("nav").$(byText("Solutions")).hover().click();
+                $(byText("Enterprise")).click();
+$("h1").shouldHave((text("Build like the best")));
+    }
+}
